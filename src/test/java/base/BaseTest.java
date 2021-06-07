f48fe44717b30cdf4c8c4f2977804612d44968fe
artyomtonoyan <artyom.tonoyan@picsart.com>
2021-06-08 01:01:57 +0400

    Updated pom.xml file

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -4,12 +4,13 @@ import org.openqa.selenium.Cookie;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import pageobjects.pages.DiscoverPage;
+
 import static setup.DriverSetup.getWebDriver;
 
 public class BaseTest {
     @BeforeMethod
     public void start() {
-        DiscoverPage homePage = new DiscoverPage();
+        new DiscoverPage();
         getWebDriver().manage().addCookie(new Cookie("cookieConsent", "1"));
         getWebDriver().navigate().refresh();
     }
